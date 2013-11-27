0afca34444b9cda794a46a1bb98f81e060f984e0
pragdave <dave@pragprog.com>
2013-11-27 16:07:01 -0600

    remove boilerplate comment

--- anagrams/lib/anagrams.ex
+++ anagrams/lib/anagrams.ex
@@ -1,9 +1,8 @@
 defmodule Anagrams do
   use Application.Behaviour
 
-  # See http://elixir-lang.org/docs/stable/Application.Behaviour.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     Anagrams.Supervisor.start_link
   end
+
 end
